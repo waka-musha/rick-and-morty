037e9d817227099d015f717940328b85905d2bfb
waka-musha <danila.lobov2409@mail.ru>
2025-09-16 00:49:12 +0300

    organize imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:ta_rick_and_morty/app/di.dart';
 import 'package:ta_rick_and_morty/app/app.dart';
+import 'package:ta_rick_and_morty/app/di.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
